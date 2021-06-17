5ffcf5624939bd15e61333e70c8578285ee8437c
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-06-17 13:34:32 +0545

    alpha-bot

---
+++ bot.py
@@ -0,0 +1,44 @@
+import os
+import discord
+from discord_slash import SlashCommand
+import requests
+
+
+TOKEN = os.environ.get('CROW_DISCORD_TOKEN')
+
+guild_ids = [854914843030585344]
+
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
+
+@slash.slash(name="rate", description="Last trade rate of TNBC.", guild_ids=guild_ids)
+async def rate(ctx):
+    r = requests.get('https://tnbcrow.pythonanywhere.com/statistics').json()
+    last_rate = int(r["results"][0]["last_rate"]) / 10000
+    await ctx.send(f"`Last Trade Rate: ${last_rate}`")
+
+
+@slash.slash(name="trades", description="Recent trades!!", guild_ids=guild_ids)
+async def trades(ctx):
+    r = requests.get('https://tnbcrow.pythonanywhere.com/recent-trades').json()
+    trades = r["results"]
+    cleaned_trades = []
+    cleaned_trades.append("| Amount | Rate |")
+
+    for trade in trades:
+        data = f"| {trade['amount']} | ${trade['rate']/10000} |"
+        cleaned_trades.append(data)
+        
+    joined_string = "\n".join(cleaned_trades)
+    
+    await ctx.send(f"```{joined_string} ```", hidden=True)
+
+
+@slash.slash(name="help", description="Crow Bot help!!", guild_ids=guild_ids)
+async def help(ctx):
+    embed=discord.Embed(title="Commands", color=discord.Color.blue())
+    embed.add_field(name="/rate", value="Last verified trade of TNBC!", inline=False)
+    embed.add_field(name="/trades", value="Recent verified trades!", inline=False)
+    await ctx.send(embed=embed, hidden=True)
+    
+client.run(TOKEN)
