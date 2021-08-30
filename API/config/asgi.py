a26a01ccb5c23b27d6564377740d6b4dced4d21e
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-08-30 16:20:49 +0545

    configured asgi for deployment

--- API/config/asgi.py
+++ API/config/asgi.py
@@ -1,7 +1,13 @@
 import os
 
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/crow bot')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+DJANGO_SETTINGS_MODULE = os.getenv('DJANGO_SETTINGS_MODULE')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', DJANGO_SETTINGS_MODULE)
 
 application = get_asgi_application()
