14ceb84a1e7f0694327df4b83bd1402c132b1c14
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-09-15 14:56:25 +0545

    added staging settings + Bot ready for beta testing

---
+++ API/config/settings/staging.py
@@ -0,0 +1,20 @@
+from .base import *  # noqa: F401
+
+
+# Project Artitecture Constants
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+# Business Logic Constants
+BANK_IP = '20.98.98.0'
+MIN_TNBC_ALLOWED = 5  # In TNBC
+TRADE_CHANNEL_ID = '887624041664512030'
+DISPUTE_CHANNEL_ID = '887624651885387797s'
+AGENT_ROLE_ID = '887624291414327296'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
