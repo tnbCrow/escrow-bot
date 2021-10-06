1869bfa481162c60e991d11a57b5826f133bb414
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-10-06 08:30:23 +0545

    order_by latest offer in /trade-offer

--- escrow/views/trade_offers.py
+++ escrow/views/trade_offers.py
@@ -6,5 +6,5 @@ from ..serializers.trade_offers import TradeOfferSerializer
 
 class TradeOfferViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
-    queryset = TradeOffer.objects.all()
+    queryset = TradeOffer.objects.all().order_by('-created_at')
     serializer_class = TradeOfferSerializer
