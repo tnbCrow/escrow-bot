0c5675a2543997480e49aa825cebab7d7aee8400
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-11-12 21:30:56 +0545

    created BotConstant model to store config data

--- core/admin.py
+++ core/admin.py
@@ -5,14 +5,14 @@ from .models.users import User, UserTransactionHistory
 from .models.wallets import ThenewbostonWallet
 from .models.scan_tracker import ScanTracker
 from .models.statistics import Statistic
-from .models.guilds import Guild
+from .models.constant import BotConstant
 
 
 # Register your models here.
 admin.site.register(Transaction)
 admin.site.register(User)
 admin.site.register(ScanTracker)
-admin.site.register(Guild)
 admin.site.register(Statistic)
 admin.site.register(UserTransactionHistory)
 admin.site.register(ThenewbostonWallet)
+admin.site.register(BotConstant)
