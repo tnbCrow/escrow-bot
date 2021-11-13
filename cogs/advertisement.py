9cab1deb7a3c68f8cd677d3e078d80821405e771
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-11-13 11:35:59 +0545

    implement /advertisement cancel command

--- cogs/advertisement.py
+++ cogs/advertisement.py
@@ -5,7 +5,6 @@ from discord_slash.utils.manage_commands import create_option
 from core.utils.shortcuts import convert_to_decimal, get_or_create_tnbc_wallet, get_or_create_discord_user, convert_to_int
 from django.conf import settings
 from asgiref.sync import sync_to_async
-from table2ascii import table2ascii, PresetStyle
 from escrow.models.advertisement import Advertisement
 from escrow.utils import create_offer_table
 
@@ -86,7 +85,7 @@ class advertisement(commands.Cog):
         offer_table = create_offer_table(5)
 
         await ctx.send(f"Open Advertisements (Escrow Protected)```{offer_table}```", hidden=True)
-    
+
     @cog_ext.cog_subcommand(base="advertisement",
                             name="all",
                             description="List all your advertisements.",
@@ -112,7 +111,7 @@ class advertisement(commands.Cog):
         else:
             embed = discord.Embed(title="Oops..", description="You got no active advertisements.", color=0xe81111)
 
-        await ctx.send(embed=embed, hidden=True)       
+        await ctx.send(embed=embed, hidden=True)
 
     @cog_ext.cog_subcommand(base="advertisement",
                             name="status",
@@ -132,9 +131,9 @@ class advertisement(commands.Cog):
 
         discord_user = get_or_create_discord_user(ctx.author.id)
 
-        if Advertisement.objects.filter(uuid_hex=advertisement_id, owner=discord_user).exists():
+        if Advertisement.objects.filter(uuid_hex=advertisement_id, owner=discord_user, status=Advertisement.OPEN).exists():
             advertisement = await sync_to_async(Advertisement.objects.get)(uuid_hex=advertisement_id)
-            
+
             embed = discord.Embed(color=0xe81111)
             embed.add_field(name='ID', value=f"{advertisement.uuid_hex}", inline=False)
             embed.add_field(name='Amount', value=convert_to_int(advertisement.amount))
@@ -145,6 +144,56 @@ class advertisement(commands.Cog):
             embed = discord.Embed(title="Error!", description="404 Not Found.", color=0xe81111)
 
         await ctx.send(embed=embed, hidden=True)
+
+    @cog_ext.cog_subcommand(base="advertisement",
+                            name="cancel",
+                            description="Cancel the particular advertisement.",
+                            options=[
+                                create_option(
+                                    name="advertisement_id",
+                                    description="Enter the advertisement id you want to check the status of.",
+                                    option_type=3,
+                                    required=True
+                                )
+                            ]
+                            )
+    async def advertisement_cancel(self, ctx, advertisement_id: str):
+
+        await ctx.defer(hidden=True)
+
+        discord_user = get_or_create_discord_user(ctx.author.id)
+        tnbc_wallet = get_or_create_tnbc_wallet(discord_user)
+
+        if Advertisement.objects.filter(uuid_hex=advertisement_id, owner=discord_user, status=Advertisement.OPEN).exists():
+
+            advertisement = await sync_to_async(Advertisement.objects.get)(uuid_hex=advertisement_id)
+
+            tnbc_wallet.locked -= advertisement.amount
+            tnbc_wallet.save()
+
+            advertisement.status = Advertisement.CANCELLED
+            advertisement.save()
+
+            offer_channel = self.bot.get_channel(int(settings.OFFER_CHANNEL_ID))
+            offer_table = create_offer_table(5)
+
+            async for oldMessage in offer_channel.history():
+                await oldMessage.delete()
+
+            await offer_channel.send(f"Open Advertisements (Escrow Protected)```{offer_table}```")
+
+            embed = discord.Embed(title="Advertisement Cancelled Successfully", description="", color=0xe81111)
+            embed.add_field(name='ID', value=f"{advertisement.uuid_hex}", inline=False)
+            embed.add_field(name='Amount', value=convert_to_int(advertisement.amount))
+            embed.add_field(name='Price Per TNBC (USDT)', value=convert_to_decimal(advertisement.price))
+            embed.add_field(name='Payment Method', value=advertisement.payment_method)
+            embed.add_field(name='Status', value=advertisement.status)
+
+        else:
+            embed = discord.Embed(title="Error!", description="404 Not Found.", color=0xe81111)
+
+        await ctx.send(embed=embed, hidden=True)
+
 
 def setup(bot):
     bot.add_cog(advertisement(bot))
