eb18e4d0f3151b7d786c34c0d2bad2637f813318
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-12-01 12:33:17 +0545

    order by price /advertisement api endpoint

--- escrow/views/advertisement.py
+++ escrow/views/advertisement.py
@@ -6,5 +6,5 @@ from ..serializers.advertisement import AdvertisementSerializer
 
 class AdvertisementViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
-    queryset = Advertisement.objects.filter(status=Advertisement.OPEN)
+    queryset = Advertisement.objects.filter(status=Advertisement.OPEN).order_by('price')
     serializer_class = AdvertisementSerializer
